acc11aa68664cecc22a886aa18907378ae810cfe
Dave Thompson <davecthompson@gmail.com>
2025-11-16 19:31:43 -0800

    Header and left nav update

--- src/features/AppHeader/AppHeader.tsx
+++ src/features/AppHeader/AppHeader.tsx
@@ -1,54 +1,9 @@
 import { useAtom } from 'jotai'
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
-import { appViewModeAtom, isToolbarCompactAtom, AppViewMode } from '../../data/atoms'
-import { Button } from '../../components/Button'
-import { Switch } from '../../components/Switch'
-import { useUndoRedo } from '../../data/useUndoRedo'
-import { useIsMac } from '../../data/useIsMac'
+import { appViewModeAtom, AppViewMode } from '../../data/atoms'
 import { AnimatedTabs, Tab } from '../../components/AnimatedTabs'
 import { AnimatedLogo } from '../../components/AnimatedLogo/AnimatedLogo'
 import { PartnerLogo } from '../../components/PartnerLogo/PartnerLogo'
 import styles from './AppHeader.module.css'
-
-const HeaderMenu = () => {
-  const { undo, redo, canUndo, canRedo } = useUndoRedo()
-  const isMac = useIsMac()
-  const [isCompact, setIsCompact] = useAtom(isToolbarCompactAtom)
-
-  return (
-    <DropdownMenu.Portal>
-      <DropdownMenu.Content
-        className="menu-popover"
-        sideOffset={8}
-        align="start"
-        onCloseAutoFocus={(e) => e.preventDefault()}
-      >
-        <DropdownMenu.Item className="menu-item" onSelect={undo} disabled={!canUndo}>
-          <span className="checkmark-container">
-            <span className="material-symbols-rounded">undo</span>
-          </span>
-          <span>Undo</span>
-          <span className="hotkey">{isMac ? '⌘Z' : 'Ctrl+Z'}</span>
-        </DropdownMenu.Item>
-        <DropdownMenu.Item className="menu-item" onSelect={redo} disabled={!canRedo}>
-          <span className="checkmark-container">
-            <span className="material-symbols-rounded">redo</span>
-          </span>
-          <span>Redo</span>
-          <span className="hotkey">{isMac ? '⇧⌘Z' : 'Ctrl+Y'}</span>
-        </DropdownMenu.Item>
-        <DropdownMenu.Separator className={styles.menuSeparator} />
-        {/* Custom item for the switch to prevent menu from closing on click */}
-        <DropdownMenu.Item className="menu-item" onSelect={(e) => e.preventDefault()}>
-          <div className={styles.menuItem}>
-            <label htmlFor="compact-toolbar-toggle">Compact Toolbar</label>
-            <Switch id="compact-toolbar-toggle" checked={isCompact} onCheckedChange={setIsCompact} />
-          </div>
-        </DropdownMenu.Item>
-      </DropdownMenu.Content>
-    </DropdownMenu.Portal>
-  )
-}
 
 export const AppHeader = () => {
   const [viewMode, setViewMode] = useAtom(appViewModeAtom)
@@ -56,15 +11,6 @@ export const AppHeader = () => {
   return (
     <header className={styles.appHeader}>
       <div className={styles.left}>
-        <DropdownMenu.Root>
-          <DropdownMenu.Trigger asChild>
-            <Button variant="tertiary" size="m" iconOnly aria-label="Main menu">
-              <span className="material-symbols-rounded">menu</span>
-            </Button>
-          </DropdownMenu.Trigger>
-          <HeaderMenu />
-        </DropdownMenu.Root>
-        <div className={styles.divider} />
         <div className={styles.logoGroup}>
           <AnimatedLogo size="header" />
           <span className="material-symbols-rounded">close</span>
